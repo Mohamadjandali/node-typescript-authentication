b7caac52ce971e1760614e8794a3040681bb6db7
Mohamadjandali <qaisaljandali944@gmail.com>
2022-02-01 02:36:50 +0200

    adding the controllers

---
+++ controllers/user.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+
+import bcrypt from "bcrypt";
+
+import { User } from "../src/entities/User";
+
+type RequestBody = {
+  username: string;
+  email: string;
+  password: string;
+  id: number;
+};
+
+export const registerUser = async (req: Request, res: Response) => {
+  const { body } = req;
+  try {
+    const { username, email, password } = body as RequestBody;
+    const user: User = User.create({
+      username,
+      email,
+      password,
+    });
+
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { body } = req;
+  const { email, password } = body as RequestBody;
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Email or Password is incorrect!");
+    }
+
+    const matchedPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchedPassword) {
+      throw new Error("Email or Password is incorrect!");
+    }
+
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
